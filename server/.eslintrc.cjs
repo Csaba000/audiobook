d5ace6d7ddafc2697d5fe785eaea08aad9e6fad8
lorandkapusi <kapusilorand@gmail.com>
2022-07-21 10:27:37 +0000

    [#2] Setup eslint configuration on backend

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': 0,
+    'import/no-duplicates': 0
+  }
+}
