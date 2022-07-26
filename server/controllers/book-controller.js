c635c9a7e864eb7f95fe8f7582058471a4d084e6
Kapusi Lorand <kapusilorand@gmail.com>
2022-07-26 12:20:18 +0300

    database filled with test data, endpoint for listing books

--- server/controllers/book-controller.js
+++ server/controllers/book-controller.js
@@ -1,16 +1,29 @@
-import bookSchema from '../service/book-service.js';
-import connectDB from '../config/dbConnect.js';
+import bookSchema from "../service/book-service.js";
+import connectDB from "../config/dbConnect.js";
 import { MongoClient } from "mongodb";
+import { faker } from "@faker-js/faker";
+import { response } from "express";
 
-function getAudioBooks (req, res, next) {
-    connectDB().
-    dbo.collection("AudioBooks").findOne({}, function(err, result) {
-        if (err) throw err;
-        console.log(result.name);
-        db.close();
-      });
+async function createRandomBooks() {
+  await connectDB();
+  for (let i = 0; i < 15; ++i) {
+    await bookSchema.create({
+      coverUrl: faker.internet.domainName(),
+      title: faker.company.companyName(),
+      author: faker.name.firstName(),
+      description: faker.lorem.paragraph(),
+      lengthInSeconds: Math.floor(Math.random() * 1000) + 1,
+    });
+  }
+}
+async function listBooks(request, response) {
+  bookSchema.find({}, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      response.json(result);
+    }
+  });
 }
 
-getAudioBooks()
-
-export default getAudioBooks
+export default listBooks;
--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,1 +1,19 @@
-import audiobooks from '../routes/user.js';
+import userSchema from "../service/user-service.js";
+import connectDB from "../config/dbConnect.js";
+import { request } from "express";
+
+const userName = req.body.username;
+const emailAddress = req.body.email;
+const password = req.body.password;
+
+async function createUser(request,res) {
+  await connectDB();
+  await userSchema.create({
+    userName: request.body.userName,
+    emailAddress:request.body.emailAddress ,
+    password: request.body.password,
+  });
+}
+
+export default createUser;
+
